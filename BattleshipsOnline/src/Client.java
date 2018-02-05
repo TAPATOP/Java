fd0b5f0e6de955dddd26a43d9b83acbd8d6ff699
TAPATOP <hristo_lv96@abv.bg>
2018-02-05 11:24:24 +0200

    Initiates Client- Server communication

--- BattleshipsOnline/src/Client.java
+++ BattleshipsOnline/src/Client.java
@@ -1,5 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Client {
     public static void main(String args[]){
-        
+        try{
+            Socket sock = new Socket("loopback", 6969);
+            PrintWriter output = new PrintWriter(sock.getOutputStream());
+            BufferedReader serverOutput = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            BufferedReader playerInput = new BufferedReader(new InputStreamReader(System.in));
+
+            output.println("Hi, server!");
+            output.flush();
+
+            String line;
+            while((line = serverOutput.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        }catch(UnknownHostException exc){
+            System.out.println("Issues locating the host");
+        }catch(IOException exc) {
+            System.out.println("Cannot connect to host");
+        }
     }
 }
--- BattleshipsOnline/src/Server.java
+++ BattleshipsOnline/src/Server.java
@@ -1,5 +1,75 @@
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Iterator;
+import java.util.Set;
+
 public class Server {
     public static void main(String args[]){
-        System.out.println("benin");
+        System.out.println("Server is working");
+        try{
+            ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+            serverSocketChannel.socket().bind(new InetSocketAddress(6969));
+            Selector selector = Selector.open();
+
+            serverSocketChannel.configureBlocking(false);
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+            while (true) {
+                int readyChannels = selector.select();
+                if (readyChannels == 0) continue;
+
+                Set<SelectionKey> selectedKeys = selector.selectedKeys();
+                Iterator<SelectionKey> keyIterator = selectedKeys.iterator();
+                while (keyIterator.hasNext()) {
+                    SelectionKey key = keyIterator.next();
+                    if(key.isAcceptable()) {
+                        ServerSocketChannel ssc = (ServerSocketChannel)key.channel();
+                        SocketChannel sc = ssc.accept();
+                        sc.configureBlocking(false);
+                        sc.register(selector, SelectionKey.OP_READ);
+                    }
+                    if (key.isReadable()) {
+                        SocketChannel chan = (SocketChannel)key.channel();
+                        ByteBuffer buffer = ByteBuffer.allocate(1024);
+                        byte[] message = new byte[1024];
+
+                        try {
+                            while (true) {
+                                buffer.clear();
+
+                                int r = chan.read(buffer);
+                                if (r <= 0) {
+                                    break;
+                                }
+                                buffer.flip();
+                                while (buffer.limit() > buffer.position()) {
+                                    System.out.print((char) buffer.get());
+                                }
+
+                                String string = "Hello, Client!";
+                                for (int i = 0; i < string.length(); i++) {
+                                    message[i] = (byte) string.charAt(i);
+                                }
+
+                                buffer.clear();
+                                buffer.put(message);
+                                buffer.flip();
+
+                                chan.write(buffer);
+                                chan.close();
+                            }
+                        }catch(IOException | CancelledKeyException exc){
+                            System.out.println("Connection to client lost!");
+                            chan.close();
+                        }
+                    }
+                    keyIterator.remove();
+                }
+            }
+        } catch(IOException a) {
+            System.out.println("IOException");
+        }
+        System.out.println("Server is stopping");
     }
-}
+} 
